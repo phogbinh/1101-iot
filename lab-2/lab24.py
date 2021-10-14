33393254a52e877109e8d7faac6c76d8215d31b2
bill-pi <phongbinh2511@gmail.com>
2021-10-14 17:38:38 +0800

    [master] Corrected lab24.

--- lab-2/lab24.py
+++ lab-2/lab24.py
@@ -4,15 +4,20 @@
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 import time
+from threading import Thread
 
 SOUND_SPEED = 34300 # centimeters / second
-BUZZER_TIME_SCALE = 10
+RANGE = [0, 8, 25]
+INTERVAL = [0.1, 0.3, 1]
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 GPIO_DHT = 4
 GPIO_BUZZER = 2
 GPIO_TRIGGER = 23
 GPIO_ECHO = 24
+
+is_blink = False
+thread = None
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_BUZZER, GPIO.OUT)
@@ -32,14 +37,38 @@ def get_distance(temperature):
   distanceTraveled = (SOUND_SPEED + 60 * temperature) * (end - start)
   return distanceTraveled / 2 # go back and forth
 
-while True:
-  humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, GPIO_DHT)
-  if humidity is not None and temperature is not None:
-    print('Temp={0:0.2f}*C Humidity={1:0.2f}%'.format(temperature, humidity))
-    distance = get_distance(temperature)
-    print('Distance: {:.1f}cm'.format(distance))
+def blink(interval):
+  while is_blink:
     GPIO.output(GPIO_BUZZER, GPIO.HIGH)
-    time.sleep(distance / BUZZER_TIME_SCALE)
+    time.sleep(interval)
     GPIO.output(GPIO_BUZZER, GPIO.LOW)
-  else:
-    print('Fail to retrieve data from sensor DHT22')
+    time.sleep(interval)
+
+try:
+  print('Press Ctrl-C to terminate the program')
+  while True:
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, GPIO_DHT)
+    if humidity is not None and temperature is not None:
+      print('Temp={0:0.2f}*C Humidity={1:0.2f}%'.format(temperature, humidity))
+      distance = get_distance(temperature)
+      print('Distance: {:.1f}cm'.format(distance))
+      interval = INTERVAL[2]
+      if (RANGE[0] < distance <= RANGE[1]):
+        interval = INTERVAL[0]
+      elif (RANGE[1] < distance <= RANGE[2]):
+        interval = INTERVAL[1]
+      if thread is not None:
+        is_blink = False
+        thread.join()
+      is_blink = True
+      thread = Thread(target=blink, args=(interval, ))
+      thread.start()
+    else:
+      print('Fail to retrieve data from sensor DHT22')
+except KeyboardInterrupt:
+  print('Program terminated')
+finally:
+  if thread is not None:
+    is_blink = False
+    thread.join()
+  GPIO.cleanup()
